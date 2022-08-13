f63a7552e3fd1de52ff6ba2f653621b85b1f26d4
Elkhan911 <955pro@mail.ru>
2022-08-13 18:32:14 +0500

    delete button

--- script.js
+++ script.js
@@ -2,83 +2,138 @@
 //******************************************** */ VARIABLES
 //************************************************************************************************************** */
 
-let input1 = document.querySelector("#input1");
-let taskAnswer11 = document.querySelector("#taskAnswer1");
-let taskAnswer12 = document.querySelector("#taskAnswer2");
+// let input1 = document.querySelector("#input1");
+// let taskAnswer11 = document.querySelector("#taskAnswer1");
+// let taskAnswer12 = document.querySelector("#taskAnswer2");
 
-let inputLength = document.querySelector("#inputLength");
-let inputWidth = document.querySelector("#inputWidth");
-let taskAnswer21 = document.querySelector("#taskAnswer21");
-let taskAnswer22 = document.querySelector("#taskAnswer22");
-let buttton1 = document.querySelector("#button1");
+// let inputLength = document.querySelector("#inputLength");
+// let inputWidth = document.querySelector("#inputWidth");
+// let taskAnswer21 = document.querySelector("#taskAnswer21");
+// let taskAnswer22 = document.querySelector("#taskAnswer22");
+// let buttonSub1 = document.querySelector("#buttonSub1");
 
-let input3 = document.querySelector("#input3");
-let taskAnswer31 = document.querySelector("#taskAnswer31");
-let taskAnswer32 = document.querySelector("#taskAnswer32");
+// let input3 = document.querySelector("#input3");
+// let taskAnswer31 = document.querySelector("#taskAnswer31");
+// let taskAnswer32 = document.querySelector("#taskAnswer32");
 
-let input4A = document.querySelector("#input4A");
-let input4B = document.querySelector("#input4B");
-let input4C = document.querySelector("#input4C");
-let taskAnswer4 = document.querySelector("#taskAnswer4");
-let buttton2 = document.querySelector("#button2");
+// let input4A = document.querySelector("#input4A");
+// let input4B = document.querySelector("#input4B");
+// let input4C = document.querySelector("#input4C");
+// let taskAnswer4 = document.querySelector("#taskAnswer4");
+// let buttonSub2 = document.querySelector("#buttonSub2");
+
+let button1 = document.querySelector("#button1");
+let button2 = document.querySelector("#button2");
+let button3 = document.querySelector("#button3");
+let button4 = document.querySelector("#button4");
+let button5 = document.querySelector("#button5");
+let button6 = document.querySelector("#button6");
+let button7 = document.querySelector("#button7");
+let button8 = document.querySelector("#button8");
+let button9 = document.querySelector("#button9");
+let button0 = document.querySelector("#button0");
+let buttonPlus = document.querySelector("#buttonPlus");
+let buttonMinus = document.querySelector("#buttonMinus");
+let buttonMultiply = document.querySelector("#buttonMultiply");
+let buttonDivide = document.querySelector("#buttonDivide");
+
+let deleteSymbolBtn = document.querySelector("#deleteSymbol");
+let clearSymbolsBtn = document.querySelector("#clearSymbols");
+
+let calculValues1 = document.querySelector("#calculValues1");
+let calculValues2 = document.querySelector("#calculValues2");
+
+let buttons = document.querySelectorAll(".calcul__buttons");
 
 //************************************************************************************************************** */
 //******************************************** */ FUNCTIONS
 //************************************************************************************************************** */
 
-input1.addEventListener("blur", function () {
-  if (isNaN(input1.value)) {
-    alert("Нужно ввести число");
-    input1.value = "";
-  }
-  if (typeof Number(input1.value) == "number") {
-    taskAnswer11.textContent =
-      "Периметр квадрата:" + "  " + Number(input1.value) * 4;
+// // First Calculator
 
-    taskAnswer12.textContent =
-      "Площадь квадрата:" + "  " + Number(input1.value) ** 2;
-  }
-});
+// input1.addEventListener("blur", function () {
+//   if (isNaN(input1.value)) {
+//     alert("Нужно ввести число");
+//     input1.value = "";
+//   }
+//   if (typeof Number(input1.value) == "number") {
+//     taskAnswer11.textContent =
+//       "Периметр квадрата:" + "  " + Number(input1.value) * 4;
 
-buttton1.addEventListener("click", areaAndPerimeterRectangle);
+//     taskAnswer12.textContent =
+//       "Площадь квадрата:" + "  " + Number(input1.value) ** 2;
+//   }
+// });
 
-function areaAndPerimeterRectangle() {
-  if (isNaN(inputLength.value) || isNaN(inputWidth.value)) {
-    alert("Нужно ввести число");
-    input1.value = "";
-  }
-  if (
-    typeof Number(inputLength.value) == "number" &&
-    typeof Number(inputWidth.value) == "number"
-  ) {
-    taskAnswer21.textContent =
-      "Периметр квадрата: " +
-      (Number(inputLength.value) + Number(inputWidth.value)) * 2;
-    taskAnswer22.textContent =
-      "Площадь квадрата: " + inputLength.value * inputWidth.value;
-  }
+// // Second Calculator
+
+// buttonSub1.addEventListener("click", areaAndPerimeterRectangle);
+
+// function areaAndPerimeterRectangle() {
+//   if (isNaN(inputLength.value) || isNaN(inputWidth.value)) {
+//     alert("Нужно ввести число");
+//     input1.value = "";
+//   }
+//   if (
+//     typeof Number(inputLength.value) == "number" &&
+//     typeof Number(inputWidth.value) == "number"
+//   ) {
+//     taskAnswer21.textContent =
+//       "Периметр квадрата: " +
+//       (Number(inputLength.value) + Number(inputWidth.value)) * 2;
+//     taskAnswer22.textContent =
+//       "Площадь квадрата: " + inputLength.value * inputWidth.value;
+//   }
+// }
+
+// // Third Calculator
+// input3.addEventListener("blur", function () {
+//   taskAnswer31.textContent =
+//     "Площадь круга:  " + ((Number(input3.value) ** 2 / 4) * Math.PI).toFixed(2);
+
+//   taskAnswer32.textContent =
+//     "Длина окружности: " + (Number(input3.value) * Math.PI).toFixed(2);
+// });
+
+// // Fourth Calculator
+// buttonSub2.addEventListener("click", function () {
+//   let perimetr =
+//     Number(input4A.value) + Number(input4B.value) + Number(input4C.value);
+
+//   let halfPerimetr = perimetr / 2;
+
+//   taskAnswer4.textContent =
+//     "Плоащь треугольника " +
+//     Math.sqrt(
+//       halfPerimetr *
+//         (halfPerimetr - Number(input4A.value)) *
+//         (halfPerimetr - Number(input4B.value)) *
+//         (halfPerimetr - Number(input4C.value))
+//     );
+// });
+
+// // Main Calculator
+
+for (let button of buttons) {
+  button.addEventListener("click", function () {
+    calculValues1.value += this.textContent;
+  });
 }
 
-input3.addEventListener("blur", function () {
-  taskAnswer31.textContent =
-    "Площадь круга:  " + ((Number(input3.value) ** 2 / 4) * Math.PI).toFixed(2);
+function deleteSymbFunc() {
+  let str = String(calculValues1.value);
+  let arr = str.split("");
+  console.log(arr);
 
-  taskAnswer32.textContent =
-    "Длина окружности: " + (Number(input3.value) * Math.PI).toFixed(2);
-});
+  arr.pop();
+  let newValue = arr.join("");
 
-buttton2.addEventListener("click", function () {
-  let perimetr =
-    Number(input4A.value) + Number(input4B.value) + Number(input4C.value);
+  calculValues1.value = newValue;
+}
 
-  let halfPerimetr = perimetr / 2;
+deleteSymbolBtn.addEventListener("click", deleteSymbFunc);
 
-  taskAnswer4.textContent =
-    "Плоащь треугольника " +
-    Math.sqrt(
-      halfPerimetr *
-        (halfPerimetr - Number(input4A.value)) *
-        (halfPerimetr - Number(input4B.value)) *
-        (halfPerimetr - Number(input4C.value))
-    );
-});
+
+function ClearSymbFunc(){
+    
+}
