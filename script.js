675d8988cfa623d7f7780e6a0766fb99dbd0e7b7
Elkhan911 <955pro@mail.ru>
2022-08-13 17:33:58 +0500

    variables

---
+++ script.js
@@ -0,0 +1,27 @@
+//************************************************************************************************************** */
+//******************************************** */ VARIABLES
+//************************************************************************************************************** */
+
+let input1 = document.querySelector("#input1");
+let taskAnswer11 = document.querySelector("#taskAnswer1");
+let taskAnswer12 = document.querySelector("#taskAnswer2");
+
+let inputLength = document.querySelector("#inputLength");
+let inputWidth = document.querySelector("#inputWidth");
+let taskAnswer21 = document.querySelector("#taskAnswer21");
+let taskAnswer22 = document.querySelector("#taskAnswer22");
+let buttton1 = document.querySelector("#button1");
+
+let input3 = document.querySelector("#input3");
+let taskAnswer31 = document.querySelector("#taskAnswer31");
+let taskAnswer32 = document.querySelector("#taskAnswer32");
+
+let input4A = document.querySelector("#input4A");
+let input4B = document.querySelector("#input4B");
+let input4C = document.querySelector("#input4C");
+let taskAnswer4 = document.querySelector("#taskAnswer4");
+let buttton2 = document.querySelector("#button2");
+
+//************************************************************************************************************** */
+//******************************************** */ FUNCTIONS
+//************************************************************************************************************** */
