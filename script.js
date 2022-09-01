ef07a513d23d4b2a79476d024ee2a3df6b149002
Elkhan911 <955pro@mail.ru>
2022-09-01 08:09:44 +0500

    minor fixes

--- script.js
+++ script.js
@@ -2,160 +2,160 @@
 //******************************************** */ VARIABLES
 //************************************************************************************************************** */
 
-const input1 = document.querySelector("#_input1");
+let input1 = document.querySelector("#_input1");
 let span11 = document.querySelector("#_span1");
 let span12 = document.querySelector("#_span2");
-const inputLength = document.querySelector("#_inputLength");
-const inputWidth = document.querySelector("#_inputWidth");
+let inputLength = document.querySelector("#_inputLength");
+let inputWidth = document.querySelector("#_inputWidth");
 let span21 = document.querySelector("#_span21");
 let span22 = document.querySelector("#_span22");
 let span23 = document.querySelector("#_span23");
 let span24 = document.querySelector("#_span24");
-const buttonSub1 = document.querySelector("#_buttonSub1");
-const input3 = document.querySelector("#_input3");
+let buttonSub1 = document.querySelector("#_buttonSub1");
+let input3 = document.querySelector("#_input3");
 let span31 = document.querySelector("#_span31");
 let span32 = document.querySelector("#_span32");
-const input4A = document.querySelector("#_input4A");
-const input4B = document.querySelector("#_input4B");
-const input4C = document.querySelector("#_input4C");
+let input4A = document.querySelector("#_input4A");
+let input4B = document.querySelector("#_input4B");
+let input4C = document.querySelector("#_input4C");
 let span4A = document.querySelector("#_span4A");
 let span4B = document.querySelector("#_span4B");
 let span4C = document.querySelector("#_span4C");
-let span4 = document.querySelector("#span4");
-const buttonSub2 = document.querySelector("#buttonSub2");
-
-const buttonPlus = document.querySelector("#buttonPlus");
-const buttonMinus = document.querySelector("#buttonMinus");
-const buttonMultiply = document.querySelector("#buttonMultiply");
-const buttonDivide = document.querySelector("#buttonDivide");
-const deleteSymbolBtn = document.querySelector("#deleteSymbol");
-const clearSymbolsBtn = document.querySelector("#clearSymbols");
-const enterBtn1 = document.querySelector("#enterBtn1");
-const enterBtn2 = document.querySelector("#enterBtn2");
-const calculValue = document.querySelector("#calculValue");
-const calculVariable1 = document.querySelector("#calculVariable1");
-const calculVariable2 = document.querySelector("#calculVariable2");
-let calcuResult = document.querySelector("#calculResult");
-const NewOperationBtn = document.querySelector("#NewOperationBtn");
-let firstvariable = document.querySelector("#firstvariable");
-let secondVariable = document.querySelector("#secondVariable");
-const buttons = document.querySelectorAll(".calcul__btns");
+let span4 = document.querySelector("#_span4");
+let buttonSub2 = document.querySelector("#_buttonSub2");
 
 //************************************************************************************************************** */
 //******************************************** */ FUNCTIONS
 //************************************************************************************************************** */
 
 // функция для проверки isNaN
-function isItNaNorEmpty(value) {
-  if (isNaN(value) || value == "" || value == " " || value == "  ") {
-    alert("Нужно ввести число");
-    input1.value = "";
-    return false;
-  } else return true;
-}
+// function isItNaNorEmpty(value) {
+//   if (isNaN(value) || value == "" || value == " " || value == "  ") {
+//     alert("Нужно ввести число");
+//     input1.value = "";
+//     return false;
+//   } else return true;
+// }
 
-// First Calculator
-input1.addEventListener("keydown", function (event) {
-  if (event.key == "Enter") {
-    if (isItNaNorEmpty(input1.value)) {
-      span11.textContent = Number(input1.value) * 4;
-      span12.textContent = Number(input1.value) ** 2;
-    }
-    input1.value = "";
-  }
-});
+// // First Calculator
+// input1.addEventListener("keydown", function (event) {
+//   if (event.key == "Enter") {
+//     if (isItNaNorEmpty(input1.value)) {
+//       span11.textContent = Number(input1.value) * 4;
+//       span12.textContent = Number(input1.value) ** 2;
+//     }
+//     input1.value = "";
+//   }
+// });
 
-// Second Calculator
-inputLength.addEventListener("keydown", function (event) {
-  if (event.key == "Enter") {
-    if (isItNaNorEmpty(inputLength.value)) {
-      span21.textContent = inputLength.value;
-    }
-    inputLength.value = "";
-  }
-});
+// // Second Calculator
+// inputLength.addEventListener("keydown", function (event) {
+//   if (event.key == "Enter") {
+//     if (isItNaNorEmpty(inputLength.value)) {
+//       span21.textContent = inputLength.value;
+//     }
+//     inputLength.value = "";
+//   }
+// });
 
-inputWidth.addEventListener("keydown", function (event) {
-  if (event.key == "Enter") {
-    if (isItNaNorEmpty(inputWidth.value)) {
-      span22.textContent = inputWidth.value;
-    }
-    inputWidth.value = "";
-  }
-});
+// inputWidth.addEventListener("keydown", function (event) {
+//   if (event.key == "Enter") {
+//     if (isItNaNorEmpty(inputWidth.value)) {
+//       span22.textContent = inputWidth.value;
+//     }
+//     inputWidth.value = "";
+//   }
+// });
 
-buttonSub1.addEventListener("click", function () {
-  span23.textContent =
-    (Number(span21.textContent) + Number(span22.textContent)) * 2;
-  span24.textContent = span21.textContent * span22.textContent;
-});
+// buttonSub1.addEventListener("click", function () {
+//   span23.textContent =
+//     (Number(span21.textContent) + Number(span22.textContent)) * 2;
+//   span24.textContent = span21.textContent * span22.textContent;
+// });
 
-// Third Calculator
-input3.addEventListener("keydown", function (event) {
-  if (event.key == "Enter") {
-    if (isItNaNorEmpty(input3.value)) {
-      span31.textContent = ((Number(input3.value) ** 2 / 4) * Math.PI).toFixed(
-        2
-      );
-      span32.textContent = (Number(input3.value) * Math.PI).toFixed(2);
-    }
-  }
-});
+// // Third Calculator
+// input3.addEventListener("keydown", function (event) {
+//   if (event.key == "Enter") {
+//     if (isItNaNorEmpty(input3.value)) {
+//       span31.textContent = ((Number(input3.value) ** 2 / 4) * Math.PI).toFixed(
+//         2
+//       );
+//       span32.textContent = (Number(input3.value) * Math.PI).toFixed(2);
+//     }
+//   }
+// });
 
-// Fourth Calculator
-input4A.addEventListener("keydown", function (event) {
-  if (event.key == "Enter") {
-    if (isItNaNorEmpty(input4A.value)) {
-      span4A.textContent = this.value;
-    }
-    this.value = "";
-  }
-});
+// // Fourth Calculator
+// input4A.addEventListener("keydown", function (event) {
+//   if (event.key == "Enter") {
+//     if (isItNaNorEmpty(input4A.value)) {
+//       span4A.textContent = this.value;
+//     }
+//     this.value = "";
+//   }
+// });
 
-input4B.addEventListener("keydown", function (event) {
-  if (event.key == "Enter") {
-    if (isItNaNorEmpty(input4B.value)) {
-      span4B.textContent = this.value;
-    }
-    this.value = "";
-  }
-});
+// input4B.addEventListener("keydown", function (event) {
+//   if (event.key == "Enter") {
+//     if (isItNaNorEmpty(input4B.value)) {
+//       span4B.textContent = this.value;
+//     }
+//     this.value = "";
+//   }
+// });
 
-input4C.addEventListener("keydown", function (event) {
-  if (event.key == "Enter") {
-    if (isItNaNorEmpty(input4C.value)) {
-      span4C.textContent = this.value;
-    }
-    this.value = "";
-  }
-});
+// input4C.addEventListener("keydown", function (event) {
+//   if (event.key == "Enter") {
+//     if (isItNaNorEmpty(input4C.value)) {
+//       span4C.textContent = this.value;
+//     }
+//     this.value = "";
+//   }
+// });
 
-buttonSub2.addEventListener("click", function () {
-  let perimetr =
-    Number(span4A.textContent) +
-    Number(span4B.textContent) +
-    Number(span4C.textContent);
+// buttonSub2.addEventListener("click", function () {
+//   let perimetr =
+//     Number(span4A.textContent) +
+//     Number(span4B.textContent) +
+//     Number(span4C.textContent);
 
-  let halfPerimetr = perimetr / 2;
+//   let halfPerimetr = perimetr / 2;
 
-  span4.textContent = Math.sqrt(
-    halfPerimetr *
-      (halfPerimetr - Number(span4A.textContent)) *
-      (halfPerimetr - Number(span4B.textContent)) *
-      (halfPerimetr - Number(span4C.textContent))
-  ).toFixed(2);
+//   span4.textContent = Math.sqrt(
+//     halfPerimetr *
+//       (halfPerimetr - Number(span4A.textContent)) *
+//       (halfPerimetr - Number(span4B.textContent)) *
+//       (halfPerimetr - Number(span4C.textContent))
+//   ).toFixed(2);
 
-  console.log(perimetr);
-  console.log(halfPerimetr);
-  console.log(Number(span4A.textContent));
-  console.log(Number(span4B.textContent));
-  console.log(Number(span4C.textContent));
-  console.log(span4.textContent);
-});
+//   console.log(perimetr);
+//   console.log(halfPerimetr);
+//   console.log(Number(span4A.textContent));
+//   console.log(Number(span4B.textContent));
+//   console.log(Number(span4C.textContent));
+//   console.log(span4.textContent);
+// });
+
+let buttonPlus = document.querySelector("#buttonPlus");
+let buttonMinus = document.querySelector("#buttonMinus");
+let buttonMultiply = document.querySelector("#buttonMultiply");
+let buttonDivide = document.querySelector("#buttonDivide");
+let deleteSymbolBtn = document.querySelector("#deleteSymbol");
+let clearSymbolsBtn = document.querySelector("#clearSymbols");
+let enterBtn1 = document.querySelector("#enterBtn1");
+let enterBtn2 = document.querySelector("#enterBtn2");
+let calculValue = document.querySelector("#calculValue");
+let calculVariable1 = document.querySelector("#calculVariable1");
+let calculVariable2 = document.querySelector("#calculVariable2");
+let calcuResult = document.querySelector("#calculResult");
+let NewOperationBtn = document.querySelector("#NewOperationBtn");
+let firstvariable = document.querySelector("#firstvariable");
+let secondVariable = document.querySelector("#secondVariable");
+let buttons = document.querySelectorAll(".btns");
 
 // Main Calculator
 // экранная клавиатура для кнопок
-for (const button of buttons) {
+for (let button of buttons) {
   button.addEventListener("click", function () {
     calculValue.value += button.textContent;
   });
